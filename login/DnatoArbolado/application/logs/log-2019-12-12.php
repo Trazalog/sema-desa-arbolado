752faadfcd2ca855f358411f2b839f227dba3506
Hugo Gallardo <hugoeduardogallardo@gmail.com>
2019-12-12 12:11:28 -0300

    Subida parcial
    - falta guardar tipo arbol y cambiar listado de arboes
    - revisar mapa no funca

---
+++ login/DnatoArbolado/application/logs/log-2019-12-12.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2019-12-12 14:06:47 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:06:48 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:06:49 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-12-12 14:06:52 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:06:52 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:06:52 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-12-12 14:06:53 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:06:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:06:53 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-12-12 14:06:53 --> cURL Class Initialized
+DEBUG - 2019-12-12 14:06:53 --> Total execution time: 0.4056
+DEBUG - 2019-12-12 14:07:49 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:07:49 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:07:49 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-12-12 14:07:49 --> cURL Class Initialized
+DEBUG - 2019-12-12 14:07:49 --> Total execution time: 0.0936
+DEBUG - 2019-12-12 14:08:13 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:08:13 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:08:13 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-12-12 14:08:13 --> cURL Class Initialized
+DEBUG - 2019-12-12 14:08:14 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:08:14 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:08:14 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-12-12 14:08:15 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:08:15 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:08:15 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-12-12 14:09:25 --> UTF-8 Support Enabled
+DEBUG - 2019-12-12 14:09:25 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-12-12 14:09:25 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+ERROR - 2019-12-12 14:09:26 --> Severity: Notice --> Undefined index: direccionsalida D:\xampp\htdocs\sema-desa-arbolado\login\DnatoArbolado\application\controllers\Main.php 780
