628bed1d5f8de726f59d7de8a24b13e233524542
Kevin Marchan <venx22@gmail.com>
2021-03-31 15:58:21 -0300

    fix reporte 1 y 2 agregado reporte 3
    cambios en maquetas y agregado de validaciones en reportes 1 y 2.
    agregado de reporte 3

--- web/application/controllers/Reporte.php
+++ web/application/controllers/Reporte.php
@@ -26,8 +26,6 @@ class Reporte extends CI_Controller {
 
 	$data['censos'] = $this->Censos->listar()->censos->censo;
 	$data['departamentos'] = $this->Departamentos->listar()->departamentos->departamento;
-	//$data['areas'] = $this->Areas->listar()->areas->area;
-	//$data['manzanas'] = $this->Manzanas->listar()->manzanas->manzana;
 	$this->load->view('reporte/listar_total',$data);	
 	}
 
@@ -35,9 +33,21 @@ class Reporte extends CI_Controller {
 
 		$data['censos'] = $this->Censos->listar()->censos->censo;
 		$data['departamentos'] = $this->Departamentos->listar()->departamentos->departamento;
-		//$data['areas'] = $this->Areas->listar()->areas->area;
-		//$data['manzanas'] = $this->Manzanas->listar()->manzanas->manzana;
-		$this->load->view('reporte/listar_gral_1',$data);	
+			$this->load->view('reporte/listar_gral_1',$data);	
+		}
+
+	function listar_gral_2(){
+
+		$data['censos'] = $this->Censos->listar()->censos->censo;
+		$data['departamentos'] = $this->Departamentos->listar()->departamentos->departamento;
+		$data['tipo_taza'] = $this->Reportes->tipo_taza()->tablas->tabla;
+		$data['listar_arbol_especie'] = $this->Reportes->listar_arbol_especie()->tablas->tabla;
+		$data['alineacion_arbol'] = $this->Reportes->alineacion_arbol()->tablas->tabla;
+		$data['estado'] = $this->Reportes->estado()->tablas->tabla;
+		$data['taza_inscrustada'] = $this->Reportes->taza_inscrustada()->tablas->tabla;
+		$data['acequia'] = $this->Reportes->acequia()->tablas->tabla;
+
+		$this->load->view('reporte/listar_gral_2',$data);	
 		}
 	
 
@@ -81,6 +91,7 @@ public function buscar_por_filtro_listar()
 		}
 }
 
+
 //buscar por filtros para reporte gral 1
 public function buscar_por_filtro_listar_gral_1()
 {
@@ -107,6 +118,54 @@ public function buscar_por_filtro_listar_gral_1()
 
 			$data['reportes'] = $this->Reportes->listar_reporte($censo_seleccionada, $fecha_desde, $fecha_hasta, $departamento, $area, $manzana)->arboles->arbol;
 			$this->load->view('reporte/listar_table_reporte_gral_1',$data);
+		}
+}
+
+
+//buscar por filtros para reporte gral 2
+public function buscar_por_filtro_listar_gral_2()
+{
+	if($_GET)
+		{
+			$censo_seleccionada = $_GET["cens_id"];
+			$fecha_desde = $_GET["fec_desde"];
+			$fecha_hasta = $_GET["fec_hasta"];
+			$departamento = $_GET["departamento"];
+			$area = $_GET["area"];
+			$manzana = $_GET["manzana"];
+
+			$calle = $_GET["calle"];
+			$tipo_taza = $_GET["tipo_taza"];
+			$especie = $_GET["especie"];
+			$aliniacion_arbol = $_GET["aliniacion_arbol"];
+			$estado_sanitario = $_GET["estado_sanitario"];
+			$tapa_taza_incrustada = $_GET["tapa_taza_incrustada"];
+			$acequia = $_GET["acequia"];
+
+		
+			$data['reportes'] = $this->Reportes->listar_reporte_gral2($censo_seleccionada, $fecha_desde, $fecha_hasta, $departamento, $area, $manzana, $calle, $tipo_taza, $especie, $aliniacion_arbol, $estado_sanitario, $tapa_taza_incrustada, $acequia)->arboles->arbol;
+			
+			$this->load->view('reporte/listar_table_reporte_gral_2',$data);
+		}
+	else	{
+		$censo_seleccionada = $this->input->post('censo_select');
+		$fecha_desde = $this->input->post('fec_desde');
+		$fecha_hasta = $this->input->post('fec_hasta');
+		$departamento = $this->input->post('departamento');
+		$area = $this->input->post('area');
+		$manzana = $this->input->post('manzana');
+
+		$calle = $this->input->post('calle');
+		$tipo_taza = $this->input->post('tipo_taza');
+		$especie = $this->input->post('especie');
+		$aliniacion_arbol = $this->input->post('aliniacion_arbol');
+		$estado_sanitario = $this->input->post('estado_sanitario');
+		$tapa_taza_incrustada = $this->input->post('tapa_taza_incrustada');
+		$acequia = $this->input->post('acequia');
+
+		$data['reportes'] = $this->Reportes->listar_reporte($censo_seleccionada, $fecha_desde, $fecha_hasta, $departamento, $area, $manzana, $calle, $tipo_taza, $especie, $aliniacion_arbol, $estado_sanitario, $tapa_taza_incrustada, $acequia)->arboles->arbol;
+			
+			$this->load->view('reporte/listar_table_reporte_gral_2',$data);
 		}
 }
 
@@ -140,6 +199,22 @@ function ManzanaXarea(){
 					
 
 	$data['manzanas'] = $this->Reportes->ManzanaXarea($area)->manzanas->manzana;
+	echo json_encode($data);
+}
+}
+
+function CallesXdepartamento(){
+	if($_GET){	
+		$departamento = $_GET["departamento"];
+
+		$data['calles'] = $this->Reportes->CallesXdepartamento($departamento)->calles->calle;
+		}
+	else	{
+
+	$departamento = $this->input->post('departamento');
+
+
+	$data['calles'] = $this->Reportes->CallesXdepartamento($departamento)->calles->calle;
 	echo json_encode($data);
 }
 }
--- web/application/models/Reportes.php
+++ web/application/models/Reportes.php
@@ -68,6 +68,175 @@ $resource = "/arboles/cens_id/$censo_seleccionada/fec_desde/$fecha_desde/fec_has
 }  
 
 
+// listado del reporte reporte gral 2
+function listar_reporte_gral2($censo_seleccionada, $fecha_desde, $fecha_hasta, $departamento, $area, $manzana, $calle, $tipo_taza, $especie, $aliniacion_arbol, $estado_sanitario, $tapa_taza_incrustada, $acequia){
+  
+  //select multiple departamento
+  $array_dptos = explode(",", $departamento);
+
+     for ($i=0;$i<count($array_dptos);$i++) 
+         {
+          $datos = "depa_id_list=".$array_dptos[$i];
+            $array_contenedor_dptos[] = $datos;
+            $str_dato_array = implode("&",$array_contenedor_dptos); 
+            $this->session->set_userdata('datos_dptos',$str_dato_array);
+           } 
+
+     $datos_dptos = $this->session->userdata('datos_dptos');
+
+      //select multiple area
+  $array_area = explode(",", $area);
+
+
+  for ($i=0;$i<count($array_area);$i++) 
+      {
+       $datos = "arge_id_list=".$array_area[$i]; 
+         $array_contenedor_areas[] = $datos;
+         $str_dato_array = implode("&",$array_contenedor_areas); 
+         $this->session->set_userdata('datos_arge',$str_dato_array);
+        } 
+
+  $datos_arge = $this->session->userdata('datos_arge');
+
+    //select multiple manzana
+    $array_manzana = explode(",", $manzana);
+
+
+    for ($i=0;$i<count($array_manzana);$i++) 
+        {
+         $datos = "manz_id_list=".$array_manzana[$i]; 
+           $array_contenedor_manzanas[] = $datos;
+           $str_dato_array = implode("&",$array_contenedor_manzanas); 
+           $this->session->set_userdata('datos_manzana',$str_dato_array);
+          } 
+  
+   $datos_manzana = $this->session->userdata('datos_manzana');
+
+
+   //////////////////////
+   //////////////////////
+
+   //select multiple calles
+$array_calle = explode(",", $calle);
+
+
+for ($i=0;$i<count($array_calle);$i++) 
+    {
+     $datos = "call_id_list=".$array_calle[$i]; 
+       $array_contenedor_calle[] = $datos;
+       $str_dato_array = implode("&",$array_contenedor_calle); 
+       $this->session->set_userdata('datos_calle',$str_dato_array);
+      } 
+
+$datos_calle = $this->session->userdata('datos_calle');
+
+
+ //select multiple tipo taza
+ $array_tipo_taza = explode(",", $tipo_taza);
+
+
+ for ($i=0;$i<count($array_tipo_taza);$i++) 
+     {
+      $datos = "taza_list=".$array_tipo_taza[$i]; 
+        $array_contenedor_tipo_taza[] = $datos;
+        $str_dato_array = implode("&",$array_contenedor_tipo_taza); 
+        $this->session->set_userdata('datos_tipo_taza',$str_dato_array);
+       } 
+
+$datos_tipo_taza = $this->session->userdata('datos_tipo_taza');
+
+
+//select multiple tipo especie
+$array_especie = explode(",", $especie);
+
+
+for ($i=0;$i<count($array_especie);$i++) 
+    {
+     $datos = "especie_id_list=".$array_especie[$i]; 
+       $array_contenedor_especie[] = $datos;
+       $str_dato_array = implode("&",$array_contenedor_especie); 
+       $this->session->set_userdata('datos_especie',$str_dato_array);
+      } 
+
+$datos_especie = $this->session->userdata('datos_especie');
+
+
+
+//select multiple aliniacion_arbol
+$array_aliniacion_arbol = explode(",", $aliniacion_arbol);
+
+
+for ($i=0;$i<count($array_aliniacion_arbol);$i++) 
+    {
+     $datos = "alineacion_list=".$array_aliniacion_arbol[$i]; 
+       $array_contenedor_aliniacion_arbol[] = $datos;
+       $str_dato_array = implode("&",$array_contenedor_aliniacion_arbol); 
+       $this->session->set_userdata('datos_aliniacion_arbol',$str_dato_array);
+      } 
+
+$datos_aliniacion_arbol = $this->session->userdata('datos_aliniacion_arbol');
+
+
+//select multiple estado_sanitario
+$array_estado_sanitario = explode(",", $estado_sanitario);
+
+
+for ($i=0;$i<count($array_estado_sanitario);$i++) 
+    {
+     $datos = "estado_list=".$array_estado_sanitario[$i]; 
+       $array_contenedor_estado_sanitario[] = $datos;
+       $str_dato_array = implode("&",$array_contenedor_estado_sanitario); 
+       $this->session->set_userdata('datos_estado_sanitario',$str_dato_array);
+      } 
+
+$datos_estado_sanitario = $this->session->userdata('datos_estado_sanitario');
+
+//select multiple tapa_taza_incrustada
+$array_tapa_taza_incrustada = explode(",", $tapa_taza_incrustada);
+
+
+for ($i=0;$i<count($array_tapa_taza_incrustada);$i++) 
+    {
+     $datos = "tapa_list=".$array_tapa_taza_incrustada[$i]; 
+       $array_contenedor_tapa_taza_incrustada[] = $datos;
+       $str_dato_array = implode("&",$array_contenedor_tapa_taza_incrustada); 
+       $this->session->set_userdata('datos_tapa_taza_incrustada',$str_dato_array);
+      } 
+
+$datos_tapa_taza_incrustada = $this->session->userdata('datos_tapa_taza_incrustada');
+
+//select multiple acequia
+$array_acequia = explode(",", $acequia);
+
+
+for ($i=0;$i<count($array_acequia);$i++) 
+    {
+     $datos = "acequia_list=".$array_acequia[$i]; 
+       $array_contenedor_acequia[] = $datos;
+       $str_dato_array = implode("&",$array_contenedor_acequia); 
+       $this->session->set_userdata('datos_acequia',$str_dato_array);
+      } 
+
+$datos_acequia = $this->session->userdata('datos_acequia');
+
+///////////
+////////////////
+
+$resource = "/arboles/cens_id/$censo_seleccionada/fec_desde/$fecha_desde/fec_hasta/$fecha_hasta?$datos_dptos&$datos_arge&$datos_manzana&$datos_calle&$datos_tipo_taza&$datos_especie&$datos_aliniacion_arbol&$estado_sanitario&$datos_tapa_taza_incrustada&$datos_acequia";
+  $url = REST_REPO.$resource;
+  $array = $this->rest->callAPI("Get", $url);
+ 
+  if($array['status'] == true){
+    log_message("DEBUG", "#reporte gral Trae datos".json_encode($array));
+    return json_decode($array['data']);
+  }
+  else{
+    log_message('ERROR', '#TRAZA | #REPORTE >> listar_reporte  >> No trae datos');
+    return;
+  }
+}  
+
+
 # Obtener Informacion de areas por Departamento
 function AreaXdepartamento($departamento)
 {
@@ -117,6 +286,94 @@ function ManzanaXarea($area)
 }
 
 
+
+# Obtener Informacion de calles por Departamento
+function CallesXdepartamento($departamento)
+{
+  $count_dptos = count($departamento);
+ 
+  
+  for ($i=0;$i<count($departamento);$i++) 
+      {
+       $datos = "depa_id_list=".$departamento[$i];
+         $array_contenedor_dptos[] = $datos;
+         $str_dato_array = implode("&",$array_contenedor_dptos); 
+        } 
+
+ $datos_dptos = $str_dato_array;
+
+  $resource = '/calles/eliminado/0?'.$datos_dptos;	 	
+  $url = REST_REPO.$resource;
+  $array = $this->rest->callAPI("Get", $url);
+
+  log_message("DEBUG", "#Reporte/CallesXdepartamento".json_encode($array));
+  return json_decode($array['data']);
+}
+
+
+
+ // // Listar tipo taza
+ public function tipo_taza()
+ {
+  log_message('DEBUG', 'tipo taza/listar'); 
+     $resource = '/tabla/tazas/eliminado/0';
+     $url = REST_REPO . $resource;
+     $array = $this->rest->callAPI("GET", $url);
+     return json_decode($array['data']);
+ } 
+
+ //obtiene arbol especie
+function listar_arbol_especie()
+{		
+  log_message('DEBUG', 'Arboles/listar'); 
+  $resource = '/tabla/tipo_arbol/eliminado/0'; 
+  $url = REST_REPO.$resource;
+  $array = $this->rest->callAPI("GET", $url);
+  return json_decode($array['data']);
+}
+
+ //obtiene alineacion arbol
+ function alineacion_arbol()
+ {		
+   log_message('DEBUG', 'alineacion arbol/listar'); 
+   $resource = '/tabla/alineacion_arbol/eliminado/0'; 
+   $url = REST_REPO.$resource;
+   $array = $this->rest->callAPI("GET", $url);
+   return json_decode($array['data']);
+ }
+
+ //obtiene estado arbol
+ function estado()
+ {		
+   log_message('DEBUG', 'estado arbol/listar'); 
+   $resource = '/tabla/estado/eliminado/0'; 
+   $url = REST_REPO.$resource;
+   $array = $this->rest->callAPI("GET", $url);
+   return json_decode($array['data']);
+ }
+
+ //obtiene tazainscrustada arbol
+ function taza_inscrustada()
+ {		
+   log_message('DEBUG', 'taza inscrustada/listar'); 
+   $resource = '/tabla/taza_inscrustada/eliminado/0'; 
+   $url = REST_REPO.$resource;
+   $array = $this->rest->callAPI("GET", $url);
+   return json_decode($array['data']);
+ }
+
+ //obtiene acequia arbol
+ function acequia()
+ {		
+   log_message('DEBUG', 'acequia/listar'); 
+   $resource = '/tabla/acequia/eliminado/0'; 
+   $url = REST_REPO.$resource;
+   $array = $this->rest->callAPI("GET", $url);
+   return json_decode($array['data']);
+ }
+
+
+//detalles del form dinamico
 function Detalles($id)
   {
 
@@ -141,6 +398,7 @@ function Detalles($id)
     return $array;
   }
 
+  // obtiene imagen de arbol del form dinamico
   function Imagen($id)
   {
     $parametros["http"]["method"] = "GET";
@@ -155,4 +413,3 @@ function Detalles($id)
   }
      
 }
-
